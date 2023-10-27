e42d9e41e5db11e99a6aea28b479665577965278
Aleksander <olek.drwal@gmail.com>
2023-10-27 20:51:47 +0200

    finished implementation

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import random
 import numpy as np
 
 
-def tournament_selection(population, function, tournament_size: int = 2):
+def tournament_selection(population, pop_fitness, tournament_size: int = 2):
     selected_individuals = []
-    sorted_population = sorted(
-        population, key=lambda indiv: function(indiv), reverse=True
+    sorted_population_w_fit = sorted(
+        zip(population, pop_fitness), key=lambda entry: entry[1], reverse=False
     )
     pop_with_ranks = [
-        (rank + 1, individual) for rank, individual in enumerate(sorted_population)
+        (rank + 1, individual)
+        for rank, (individual, _) in enumerate(sorted_population_w_fit)
     ]
 
     for _ in range(len(population)):
@@ -26,10 +27,14 @@ def tournament_selection(population, function, tournament_size: int = 2):
             )
             for rank, individual in tournament
         ]
-        winner = np.random.choice(
+        # winner = np.random.choice(
+        #     [indiv for _, indiv, _ in tournament_probabilities],
+        #     p=[prob for _, _, prob in tournament_probabilities],
+        # )
+        winner = random.choices(
             [indiv for _, indiv, _ in tournament_probabilities],
-            p=[prob for _, _, prob in tournament_probabilities],
-        )
+            weights=[prob for _, _, prob in tournament_probabilities],
+        )[0]
         selected_individuals.append(winner)
 
     return selected_individuals
@@ -59,24 +64,38 @@ def init_population(
 
 
 def main(target_function, population_size: int = 100, mutation_magnitude: float = 0.1):
-    BUDGET = 10000
+    BUDGET = 100000
     iter_limit = BUDGET / population_size
     curr_iter = 0
 
-    population = init_population(population_size, 10, -100, 100)
+    curr_population = init_population(population_size, 2, -100, 100)
+    curr_pop_fitness = [target_function(individual) for individual in curr_population]
+    best_indiv, best_fitness = min(
+        zip(curr_population, curr_pop_fitness), key=lambda x: x[1]
+    )
 
     while curr_iter < iter_limit:
-        population = tournament_selection(
-            population, target_function, tournament_size=2
+        new_population = tournament_selection(
+            curr_population, curr_pop_fitness, tournament_size=2
         )
-        population = mutate(population, mutation_magnitude=mutation_magnitude)
+        new_population = mutate(new_population, mutation_magnitude=mutation_magnitude)
+        new_pop_fitness = [target_function(individual) for individual in new_population]
+        new_best_indiv, new_best_fitness = min(
+            zip(new_population, new_pop_fitness), key=lambda x: x[1]
+        )
+        if new_best_fitness < best_fitness:
+            best_fitness = new_best_fitness
+            best_indiv = new_best_indiv
+
         curr_iter += 1
+        curr_population = new_population
+
+    print(f"Best individual: {best_indiv}, fitness: {best_fitness}")
 
 
-def test():
-    xd = np.array([4, 2, 3])
-    xd2 = sorted(xd, key=lambda x: x, reverse=True)
-    print(xd2)
+def booth_function(individual):
+    x, y = individual
+    return (x + 2 * y - 7) ** 2 + (2 * x + y - 5) ** 2
 
 
-test()
+main(target_function=booth_function, population_size=100, mutation_magnitude=0.1)
